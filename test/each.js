4dd04642334cd7226abe61a821189a9ed3bf69f3
hokaccha <k.hokamura@gmail.com>
2012-03-12 15:26:08 +0900

    Fix `.each` can set Array

--- test/each.js
+++ test/each.js
@@ -2,7 +2,7 @@ var nodeunit = require('nodeunit');
 var chain = require('../index');
 
 module.exports = nodeunit.testCase({
-  '.each()': function(t) {
+  '.each() object': function(t) {
     var start = Date.now();
     chain(function(next) {
       next(null, { foo: 'bar', hoge: 'fuga' });
@@ -20,7 +20,25 @@ module.exports = nodeunit.testCase({
     })
     .end(t.done);
   },
-  '.eachParallel()': function(t) {
+  '.each() array': function(t) {
+    var start = Date.now();
+    chain(function(next) {
+      next(null, ['foo', 'bar']);
+    })
+    .each(function(key, val, next) {
+      setTimeout(function() {
+        next(null, key + '/' + val);
+      }, 100);
+    })
+    .chain(function(results, next) {
+      t.equal(results.length, 2);
+      t.equal(results[0], '0/foo');
+      t.equal(results[1], '1/bar');
+      next();
+    })
+    .end(t.done);
+  },
+  '.eachParallel() object': function(t) {
     var start = Date.now();
     chain(function(next) {
       next(null, { foo: 'bar', hoge: 'fuga'});
@@ -38,7 +56,26 @@ module.exports = nodeunit.testCase({
     })
     .end(t.done);
   },
-  'Chain.each()': function(t) {
+  '.eachParallel() array': function(t) {
+    var start = Date.now();
+    chain(function(next) {
+      next(null, ['foo', 'bar']);
+    })
+    .eachParallel(function(key, val, next) {
+      setTimeout(function() {
+        next(null, key + '/' + val);
+      }, 100);
+    })
+    .chain(function(results, next) {
+      t.equal(results.length, 2);
+      t.equal(results[0], '0/foo');
+      t.equal(results[1], '1/bar');
+      t.ok( Date.now() - start < 190 );
+      next();
+    })
+    .end(t.done);
+  },
+  'Chain.each() object': function(t) {
     var start = Date.now();
     chain.each({ foo: 'bar', hoge: 'fuga' }, function(key, val, next) {
       setTimeout(function() {
@@ -53,7 +90,23 @@ module.exports = nodeunit.testCase({
     })
     .end(t.done);
   },
-  'Chain.eachParallel()': function(t) {
+  'Chain.each() array': function(t) {
+    var start = Date.now();
+    chain.each(['foo', 'bar'], function(key, val, next) {
+      setTimeout(function() {
+        next(null, key + '/' + val);
+      }, 100);
+    })
+    .chain(function(results, next) {
+      t.equal(results.length, 2);
+      t.equal(results[0], '0/foo');
+      t.equal(results[1], '1/bar');
+      t.ok( Date.now() - start >= 190 );
+      next();
+    })
+    .end(t.done);
+  },
+  'Chain.eachParallel() object': function(t) {
     var start = Date.now();
     chain.eachParallel({ foo: 'bar', hoge: 'fuga'}, function(key, val, next) {
       setTimeout(function() {
@@ -63,6 +116,22 @@ module.exports = nodeunit.testCase({
     .chain(function(results, next) {
       t.equal(results.foo, 'foo/bar');
       t.equal(results.hoge, 'hoge/fuga');
+      t.ok( Date.now() - start < 190 );
+      next();
+    })
+    .end(t.done);
+  },
+  'Chain.eachParallel() array': function(t) {
+    var start = Date.now();
+    chain.eachParallel(['foo', 'bar'], function(key, val, next) {
+      setTimeout(function() {
+        next(null, key + '/' + val);
+      }, 100);
+    })
+    .chain(function(results, next) {
+      t.equal(results.length, 2);
+      t.equal(results[0], '0/foo');
+      t.equal(results[1], '1/bar');
       t.ok( Date.now() - start < 190 );
       next();
     })
